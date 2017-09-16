6190e21529f40a3ce0345c22ec4ed916899ca608
Evgeniy Shershnev <joshuan@yandex-team.ru>
2017-09-16 16:29:53 +0500

    With react

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -1,1 +1,9 @@
-export default 'logo';
+import React from 'react';
+
+export default class Logo extends React.Component {
+    render() {
+        return (
+            <h1>Logo h1!</h1>
+        );
+    }
+};
